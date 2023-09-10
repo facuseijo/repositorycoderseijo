ac83e3c6a83f9cd50d60c8b538edab168a0bf218
facuseijo <136520267+facuseijo@users.noreply.github.com>
2023-09-10 04:27:35 -0300

    carro

---
+++ js/products.js
@@ -0,0 +1,77 @@
+
+
+
+const productos = [
+    {
+       id:1,
+       nombre: "Paleta Cocida Cagnoli",
+       precio: 1840,
+       img:   "../images/fiambre_de_cerdo.png",
+    },
+    {
+       id:2,
+       nombre: "Jamon Cocido Cagnoli",
+       precio: 3400,
+       img: "../images/jamoncocido_Cagnoli.png",
+    },
+    {
+       id:3,
+       nombre: "Jamon Natural Cagnoli",
+       precio: 4900,
+       img:  "../images/natu.jpeg",
+    },
+    {
+       id:4,
+       nombre: "Lomo Ahumado Lario",
+       precio: 3810,
+       img: "../images/lomo.jpg",
+    },
+    {
+       id:5,
+       nombre: "Mortadela Bocha Calchaqui",
+       precio: 2110,
+       img:  "../images/bocha.png",
+    },
+    {
+       id:6,
+       nombre: "Panceta Ahumada Bierzo",
+       precio: 4500,
+       img:  "../images/pancetabierzo.jpg",
+    },
+    {
+       id:7,
+       nombre: "Salamines Cagnoli",
+       precio: 5380,
+       img: "../images/salamines.jpg",
+    },
+    {
+       id:8,
+       nombre: "Salame Cagnoli",
+       precio: 4060,
+       img: "../images/milan.png",
+    },
+    {
+       id:9,
+       nombre: "Jamon Crudo Weber",
+       precio: 4110,
+       img: "../images/crudo.jpg",
+    },
+    {
+       id:10,
+       nombre: "Cantimpalo Lario",
+       precio: 3810,
+       img: "../images/cantimpalo.jpg",
+    },
+    {
+       id:11,
+       nombre: "Holanda Varense",
+       precio: 2900,
+       img: "../images/varense.jpeg",
+    },
+    {
+       id:12,
+       nombre: "Reggiano Silvia",
+       precio: 3580,
+       img: "../images/reggiano.jpg",
+    },
+];
