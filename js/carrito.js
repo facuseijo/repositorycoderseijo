9453f33c750c11c37acfd060094a2f54f5aa35e7
facuseijo <136520267+facuseijo@users.noreply.github.com>
2023-09-24 21:05:07 -0300

    renders

---
+++ js/carrito.js
@@ -0,0 +1,64 @@
+
+renderCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+	<h3 class="modal-Header-title">Carrito.</h3>
+	`;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h3");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.Style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+		 <img src="${product.img}">
+		 <h3>${product.nombre}</h3>
+		 <p>${product.precio}$</p>
+		`;
+
+        modalContainer.append(carritoContent);
+
+        let eliminar = document.createElement("span");
+        eliminar.innerText = "✖";
+        eliminar.className = "delete-product";
+        carritoContent.append(eliminar);
+
+        eliminar.addEventListener("click", eliminarProducto);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuy = document.createElement("div");
+    totalBuy.classname = "total-content";
+    totalBuy.innerHTML = `Total a pagar: ${total} $`;
+    modalContainer.append(totalBuy);
+
+
+};
+
+verCarrito.addEventListener("click", renderCarrito);
+
+const eliminarProducto = () => {
+    const foundId = carrito.find((element) => element.id);
+
+    carrito = carrito.filter((carritoId) => {
+    return carritoId !== foundId;
+    });
+
+   renderCarrito();
+};
+
+
